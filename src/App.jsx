6ece7b8c5cb3242010ba5ed5562149a31d3d08e0
Alaa Yasser <alaayasser2018@gmail.com>
2024-04-02 20:39:02 +0200

    Create Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
-// App.js
-import { useSelector } from "react-redux";
-import { selectSidebarState } from "./context/sidebarSlice";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
-import { SidebarApp } from "./components";
-
+import { Navbar } from "./components";
 function App() {
-  const { isCollapsed } = useSelector(selectSidebarState);
-
-
   return (
     <BrowserRouter>
-      <div className="flex">
-        <div className={`${isCollapsed ? "w-sidebarCollapsed" : "w-sidebar"}`}>
-          <SidebarApp />
-        </div>
-        <div className={`${isCollapsed ? "w-contentCollapsed" : "w-content"}`}>
-          <Router />
-        </div>
-      </div>
+      <Navbar />
+      <Router />
     </BrowserRouter>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,206 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import {
+  FaEnvelope,
+  FaPhone,
+  FaInstagram,
+  FaTwitter,
+  FaFacebook,
+  FaWhatsapp,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { GrLanguage } from "react-icons/gr";
+import { Fade as Hamburger } from "hamburger-react";
+
+import styles from "../styles/navbar.module.css";
+
+const Navbar = () => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 50) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  });
+
+  const handleDropdownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+  return (
+    <>
+      <div
+        className="overlay"
+        style={{ display: isMenuOpen ? "block" : "none" }}
+        onClick={() => setIsMenuOpen(false)}
+      />
+      <nav className={styles.contactNav}>
+        <div className={`${styles.container} container`}>
+          <ul>
+            <li>
+              <span>تواصل معنا عبر</span>
+            </li>
+            <li>
+              <a href="mailto:example@example.com">
+                <span className={styles.nav_icon}>
+                  <FaEnvelope />
+                </span>
+                <span>example@example.com</span>
+              </a>
+            </li>
+            <li>
+              <a href="tel:+1234567890">
+                <span className={styles.nav_icon}>
+                  <FaPhone />
+                </span>
+                <span>+1234567890</span>
+              </a>
+            </li>
+          </ul>
+          <ul className={styles.social_links}>
+            <li>
+              <a
+                href="https://www.whatsapp.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaWhatsapp />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.facebook.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaFacebook />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.twitter.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaTwitter />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.instagram.com"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <FaInstagram />
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <nav className={`${styles.mainNav} ${isScrolled && styles.fixedNav}`}>
+        <div className={`${styles.container} container`}>
+          <div className={styles.logo}>
+            <Link to="/">
+              <img src="/path/to/logo.png" alt="Logo" />
+            </Link>
+          </div>
+
+          <div className={styles.menuToggle}>
+            {
+              <Hamburger
+                direction="right"
+                toggled={isMenuOpen}
+                toggle={setIsMenuOpen}
+                size={24}
+              />
+            }
+          </div>
+
+          <ul
+            className={`${styles.links} ${isMenuOpen && styles.activeSideMenu}`}
+          >
+            <div className={styles.menuToggle}>
+              {
+                <Hamburger
+                  direction="right"
+                  toggled={isMenuOpen}
+                  toggle={setIsMenuOpen}
+                  size={24}
+                />
+              }
+            </div>
+            <li>
+              <Link to="/">الرئيسية</Link>
+            </li>
+            <li>
+              <Link to="/about">من نحن</Link>
+            </li>
+            <li>
+              <div
+                className={styles.dropdown}
+                onClick={handleDropdownToggle}
+                onMouseEnter={() => setIsDropdownOpen(true)}
+              >
+                <Link to="/services">خدماتنا</Link>
+                {isDropdownOpen && (
+                  <div
+                    onMouseLeave={() => setIsDropdownOpen(false)}
+                    className={`${styles.dropdownContent} ${styles.activeDropdownContent}`}
+                  >
+                    <ul>
+                      <li>
+                        <Link to="/services/web">تطوير المواقع</Link>
+                      </li>
+                      <li>
+                        <Link to="/services/apps">تطوير التطبيقات</Link>
+                      </li>
+                      <li>
+                        <Link to="/services/marketing">التسويق الإلكتروني</Link>
+                      </li>
+                    </ul>
+                  </div>
+                )}
+              </div>
+            </li>
+            <li>
+              <Link to="/projects">مشاريعنا</Link>
+            </li>
+            <li>
+              <Link to="/contact">اتصل بنا</Link>
+            </li>
+
+            <li>
+              <button className={styles.languageToggle}>
+                <GrLanguage />
+              </button>
+            </li>
+
+            <div className={styles.menu_logo}>
+              {
+                <div className={styles.logo}>
+                  <Link to="/">
+                    <img src="/path/to/logo.png" alt="Logo" />
+                  </Link>
+                </div>
+              }
+            </div>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
