efba371fb76c308849699ab12b7be2057f0e9f08
Alaa Yasser <alaayasser2018@gmail.com>
2024-04-04 13:50:56 +0200

    Add Swiper component and ReviewsHome component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,11 @@
-import { HeaderHome, AboutHome, ServicesHome } from "../components";
+import { HeaderHome, AboutHome, ServicesHome,ReviewsHome } from "../components";
 const Home = () => {
   return (
     <>
       <HeaderHome />
       <AboutHome />
       <ServicesHome />
+      <ReviewsHome />
     </>
   );
 };
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as HeaderHome } from "./home/Header";
 export { default as AboutHome } from "./home/About";
 export { default as ServicesHome } from "./home/Services";
+export { default as ReviewsHome } from "./home/Reviews";
